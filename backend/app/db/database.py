da82575ebce9dbc63073e165c52fe8ae29b01ea5
Priyanshu-010 <priyanshurai2772@gmail.com>
2026-09-10 17:01:51 +0530

    DB connection with alembic an dmaking user schemas

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,4 +1,8 @@
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import (
+  AsyncSession,
+  async_sessionmaker,
+  create_async_engine,
+)
 
 from app.core.config import settings
 
@@ -7,3 +11,14 @@ engine = create_async_engine(
   settings.database_url,
   echo=True,
 )
+
+AsyncSessionLocal = async_sessionmaker(
+  bind=engine,
+  class_=AsyncSession,
+  expire_on_commit=False,
+)
+
+
+async def get_db():
+  async with AsyncSessionLocal() as session:
+    yield session
